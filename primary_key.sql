7e96b57e19548bcfea7b20e8a18a3671fa012da6
Ali Titiz <mehmetalititiz3@gmail.com>
2026-05-10 04:48:02 +0200

    primary key usage was learned

---
+++ primary_key.sql
@@ -0,0 +1,10 @@
+ -- CREATE TABLE transaction(
+--  	transaction_id INT PRIMARY KEY, -- primary key is unique identifier it can't be null nor it can't have same value like uniqe like both of them are forbidden in this case 
+     --   amount DECIMAL(5,2)
+
+-- );
+
+-- INSERT INTO transaction
+-- VALUES(NULL,2.25), (4,30); This is the error null cannot be accepted and also 4 cannot be accapted because 4 is already exist
+
+SELECT * FROM transaction;
