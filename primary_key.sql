838f50a29cab3fd81b864677df75b5f475d149b0
Ali Titiz <mehmetalititiz3@gmail.com>
2026-05-10 04:56:55 +0200

    primary key constraint was learned

--- primary_key.sql
+++ primary_key.sql
@@ -7,4 +7,10 @@
 -- INSERT INTO transaction
 -- VALUES(NULL,2.25), (4,30); This is the error null cannot be accepted and also 4 cannot be accapted because 4 is already exist
 
+
+-- ALTER TABLE transaction  ! if you forgot to add at first , you can add later like that
+-- ADD CONSTRAINT
+-- PRIMARY KEY(transaction_id);
+
+
 SELECT * FROM transaction;
